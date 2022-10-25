b0a8a2114eb5c7d091f28a36aaf85c47d491dd28
mohjohfox <marcorobinf@gmail.com>
2022-10-25 18:50:18 +0200

    fixed typo

--- src/main/java/de/dhbw/karlsruhe/services/UserService.java
+++ src/main/java/de/dhbw/karlsruhe/services/UserService.java
@@ -1,57 +1,58 @@
 package de.dhbw.karlsruhe.services;
 
-import de.dhbw.karlsruhe.adapter.UserAdapter;
-import de.dhbw.karlsruhe.model.User;
-import de.dhbw.karlsruhe.ports.UserPort;
 import java.security.NoSuchAlgorithmException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import de.dhbw.karlsruhe.adapter.UserAdapter;
+import de.dhbw.karlsruhe.model.User;
+import de.dhbw.karlsruhe.ports.UserPort;
+
 public class UserService {
 
-  private final EncryptionService encryptionService = new EncryptionService();
-  private final UserPort userPort = new UserAdapter();
+	private final EncryptionService encryptionService = new EncryptionService();
+	private final UserPort userPort = new UserAdapter();
 
-  public void saveUser(User user) {
-    userPort.saveUser(user);
-  }
+	public void saveUser(User user) {
+		userPort.saveUser(user);
+	}
 
-  public void createUser(User createUser) throws NoSuchAlgorithmException {
+	public void createUser(User createUser) throws NoSuchAlgorithmException {
 
-    if (isUserNameFree(createUser.getUserName()) && isUserNameWithoutSpecialCharacters(createUser.getUserName()) && isPasswordWithoutSpecialCharacters(createUser.getPassword())) {
-      String encryptPassword = encryptionService.getSHAEncryptedPassword(createUser.getPassword());
-      saveUser(new User(createUser.getUserName(), encryptPassword));
-    } else {
-      System.err.println("Username is already forgiven or username / password contains forbidden characters!");
-    }
+		if (isUserNameFree(createUser.getUserName()) && isUserNameWithoutSpecialCharacters(createUser.getUserName())
+				&& isPasswordWithoutSpecialCharacters(createUser.getPassword())) {
+			String encryptPassword = encryptionService.getSHAEncryptedPassword(createUser.getPassword());
+			saveUser(new User(createUser.getUserName(), encryptPassword));
+		} else {
+			System.err.println("Username is already assigned or username / password contains forbidden characters!");
+		}
 
-  }
+	}
 
-  public boolean isPasswordCorrect(User loginUser)
-      throws NoSuchAlgorithmException {
-    return !getPasswordFromUserName(loginUser.getUserName()).isBlank()
-        && encryptionService.getSHAEncryptedPassword(loginUser.getPassword())
-        .equals(getPasswordFromUserName(loginUser.getUserName()));
-  }
+	public boolean isPasswordCorrect(User loginUser) throws NoSuchAlgorithmException {
+		return !getPasswordFromUserName(loginUser.getUserName()).isBlank()
+				&& encryptionService.getSHAEncryptedPassword(loginUser.getPassword())
+						.equals(getPasswordFromUserName(loginUser.getUserName()));
+	}
 
-  private String getPasswordFromUserName(String userName) {
-    return userPort.getPassword(userName);
-  }
+	private String getPasswordFromUserName(String userName) {
+		return userPort.getPassword(userName);
+	}
 
-  private boolean isUserNameFree(String userName) {
-    return !userPort.getAllUserNames().contains(userName);
-  }
+	private boolean isUserNameFree(String userName) {
+		return !userPort.getAllUserNames().contains(userName);
+	}
 
-  private boolean isUserNameWithoutSpecialCharacters(String userName) {
-    Pattern p = Pattern.compile("[^a-zA-Z0-9]", Pattern.CASE_INSENSITIVE);
-    Matcher m = p.matcher(userName);
-    return !m.find();
-  }
+	private boolean isUserNameWithoutSpecialCharacters(String userName) {
+		Pattern p = Pattern.compile("[^a-zA-Z0-9]", Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher(userName);
+		return !m.find();
+	}
 
-  private boolean isPasswordWithoutSpecialCharacters(String password) {
-    Pattern p = Pattern.compile("&=", Pattern.CASE_INSENSITIVE);
-    Matcher m = p.matcher(password);
-    return !m.find();
-  }
+	private boolean isPasswordWithoutSpecialCharacters(String password) {
+		Pattern p = Pattern.compile("&=", Pattern.CASE_INSENSITIVE);
+		Matcher m = p.matcher(password);
+		return !m.find();
+	}
 
 }
