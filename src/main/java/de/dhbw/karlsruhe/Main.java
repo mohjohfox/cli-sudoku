60ae4ac9398b9945826f78e79d6b99a2ea4a828b
rhyperio <rhyperio.yt@gmail.com>
2022-11-01 19:34:33 +0100

    Add menu view

--- src/main/java/de/dhbw/karlsruhe/Main.java
+++ src/main/java/de/dhbw/karlsruhe/Main.java
@@ -1,16 +1,20 @@
 package de.dhbw.karlsruhe;
 
+import de.dhbw.karlsruhe.services.MenuDialogService;
+
 import de.dhbw.karlsruhe.services.StartUpDialogService;
 
 public class Main {
 
 	public static void main(String[] args) {
 		StartUpDialogService startUpDialogService = new StartUpDialogService();
+        MenuDialogService menuDialogService = new MenuDialogService();
 
 		boolean successfulLogin = startUpDialogService.signIn();
 
 		if (successfulLogin) {
 			System.out.println("You have successfully logged in!");
+            menuDialogService.startMenuDialog();
 		} else {
 			System.out.println("Login failed!");
 		}
