5f2b608d2b3bdf8ae47567875fb19d9a3933260c
rhyperio <rhyperio.yt@gmail.com>
2022-11-10 12:20:52 +0100

    implement logout feature

--- src/main/java/de/dhbw/karlsruhe/Main.java
+++ src/main/java/de/dhbw/karlsruhe/Main.java
@@ -1,21 +1,27 @@
 package de.dhbw.karlsruhe;
 
+import de.dhbw.karlsruhe.services.LogoutService;
 import de.dhbw.karlsruhe.services.MenuDialogService;
 import de.dhbw.karlsruhe.services.StartUpDialogService;
 
 public class Main {
 
 	public static void main(String[] args) {
+		
+		LogoutService logoutService = new LogoutService();
 		StartUpDialogService startUpDialogService = new StartUpDialogService();
         MenuDialogService menuDialogService = new MenuDialogService();
 
-		boolean successfulLogin = startUpDialogService.signIn();
+		while (!logoutService.getSignedIn()) {
+			boolean successfulLogin = startUpDialogService.signIn();
 
-		if (successfulLogin) {
-			System.out.println("You have successfully logged in!");
-            menuDialogService.startMenuDialog();
-		} else {
-			System.out.println("Login failed!");
+			if (successfulLogin) {
+				logoutService.setSignedIn(true);
+				System.out.println("You have successfully logged in!");
+				menuDialogService.startMenuDialog();
+			} else {
+				System.out.println("Login failed!");
+			}
 		}
 	}
 }
