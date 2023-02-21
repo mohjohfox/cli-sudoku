5df0adb04a520ff3ee39ca90c0cb0bf4ffbb8834
rhyperio <rhyperio.yt@gmail.com>
2023-02-21 19:41:40 +0100

    Adapted reviews and improved logic

--- src/main/java/de/dhbw/karlsruhe/Main.java
+++ src/main/java/de/dhbw/karlsruhe/Main.java
@@ -7,10 +7,23 @@ import de.dhbw.karlsruhe.services.StartUpDialogService;
 public class Main {
 
 	public static void main(String[] args) {
+		boolean desireToRun = true;
 		LogoutService logoutService = new LogoutService();
 		StartUpDialogService startUpDialogService = new StartUpDialogService();
         MenuDialogService menuDialogService = new MenuDialogService();
 
+
+		do {
+			boolean successfullLogin = startUpDialogService.signIn();
+			logoutService.setSignedIn(successfullLogin);
+			menuDialogService.startMenuDialog();
+
+			desireToRun = logoutService.checkDesireToRun();
+		} while (desireToRun);
+
+
+
+		/**
 		while (!logoutService.getSignedIn()) {
 			boolean successfulLogin = startUpDialogService.signIn();
 
@@ -21,6 +34,6 @@ public class Main {
 			} else {
 				System.out.println("Login failed!");
 			}
-		}
+		} */
 	}
 }
--- src/main/java/de/dhbw/karlsruhe/services/LogoutService.java
+++ src/main/java/de/dhbw/karlsruhe/services/LogoutService.java
@@ -3,9 +3,11 @@ package de.dhbw.karlsruhe.services;
 public class LogoutService {
 
     private static boolean signedIn;
+    private ScannerService scannerService;
 
     public LogoutService() {
         this.signedIn = false;
+        scannerService = new ScannerService();
     }
 
     public void logout() {
@@ -20,4 +22,18 @@ public class LogoutService {
     public void setSignedIn(boolean pSignedIn) {
         this.signedIn = pSignedIn;
     }
+
+    public boolean checkDesireToRun() {
+        System.out.println("----------------------------------------");
+        System.out.println("Do you want to re login? y/n");
+
+        String userInput = this.scannerService.getLine();
+
+        if (userInput.equalsIgnoreCase("y")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
 }
