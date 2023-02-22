44b033fb07a1d9c0f658e63776763a2385bc43f8
rhyperio <rhyperio.yt@gmail.com>
2023-02-22 09:06:40 +0100

    Create Enum for menu options and adaptions according to reviews

--- src/main/java/de/dhbw/karlsruhe/services/MenuDialogService.java
+++ src/main/java/de/dhbw/karlsruhe/services/MenuDialogService.java
@@ -5,18 +5,32 @@ import java.util.Scanner;
 
 public class MenuDialogService {
     private Scanner scanner;
-    private String[] menuOptions;
     private int userInput;
     private PlayDialogService playDialogService;
     private LeaderboardDialogService leaderboardDialogService;
     private LogoutService logoutService;
 
+    public enum MenuOptions {
+        PLAY("Play"),
+        LEADERBOARD("Leaderboard"),
+        LOGOUT("Logout");
+
+        String representation;
+        MenuOptions(String representation) {
+            this.representation = representation;
+        }
+
+        public String getRepresentation() {
+            return representation;
+        }
+    }
+
     public MenuDialogService() {
         this.scanner = new Scanner(System.in);
-        this.menuOptions = new String[]{"Play", "Leaderboard", "Logout"};
     }
 
     public void startMenuDialog() {
+        // ToDo: Check login status to Display Menu till logout
         this.displayMenuOptions();
         this.userInput = this.awaitUserInput();
         this.checkUserInput(this.userInput);
@@ -24,8 +38,8 @@ public class MenuDialogService {
 
     private void displayMenuOptions() {
         System.out.println("Welcome to your favorite cli Sudoku :)");
-        for (int i=0; i < this.menuOptions.length; i++) {
-            System.out.println("[" + (i+1) + "] " + this.menuOptions[i]);
+        for (int i=0; i < MenuOptions.values().length; i++) {
+            System.out.println("[" + (i+1) + "] " + MenuOptions.values()[i].getRepresentation());
         }
     }
 
@@ -35,7 +49,7 @@ public class MenuDialogService {
         while (input == -1) {
             try {
                 input = this.scanner.nextInt();
-                if (!(input > 0 & input <= this.menuOptions.length)) {
+                if (!(input > 0 & input <= MenuOptions.values().length)) {
                     input = -1;
                     System.out.println("Invalid Input - Please enter a valid number!");
                 }
