0387494b73638b31ad8219d14ef124584f1673e9
Marcel Sisic <marcel.sisic@sick.de>
2023-05-23 11:50:28 +0200

    Add classes to store timestamp

---
+++ src/main/java/de/dhbw/karlsruhe/adapters/persistence/TimetrackAdapter.java
@@ -0,0 +1,7 @@
+package de.dhbw.karlsruhe.adapters.persistence;
+
+import de.dhbw.karlsruhe.domain.ports.persistence.TimetrackPort;
+
+public class TimetrackAdapter extends AbstractStoreAdapter implements TimetrackPort {
+
+}
