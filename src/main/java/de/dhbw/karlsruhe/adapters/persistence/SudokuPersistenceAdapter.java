2646a70e7c53f4ef9cd4607307a6a9286465362e
mohjohfox <marcorobinf@gmail.com>
2023-05-29 16:36:45 +0200

    added sudoku-builder

--- src/main/java/de/dhbw/karlsruhe/adapters/persistence/SudokuPersistenceAdapter.java
+++ src/main/java/de/dhbw/karlsruhe/adapters/persistence/SudokuPersistenceAdapter.java
@@ -3,230 +3,240 @@ package de.dhbw.karlsruhe.adapters.persistence;
 import de.dhbw.karlsruhe.domain.Location;
 import de.dhbw.karlsruhe.domain.models.Difficulty;
 import de.dhbw.karlsruhe.domain.models.Sudoku;
+import de.dhbw.karlsruhe.domain.models.SudokuBuilder;
 import de.dhbw.karlsruhe.domain.models.SudokuSaveEntry;
 import de.dhbw.karlsruhe.domain.models.wrapper.SudokuArray;
 import de.dhbw.karlsruhe.domain.ports.persistence.SudokuPersistencePort;
 import de.dhbw.karlsruhe.domain.wrappers.DateWrapper;
-
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.nio.file.Files;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public class SudokuPersistenceAdapter extends AbstractStoreAdapter implements SudokuPersistencePort {
 
-    private final String SUDOKUFILENAME = "SudokuStoreFile";
+  private final String SUDOKUFILENAME = "SudokuStoreFile";
 
-    public SudokuPersistenceAdapter(Location filePath) {
-        super(filePath);
+  public SudokuPersistenceAdapter(Location filePath) {
+    super(filePath);
+  }
+
+  @Override
+  public void saveSudoku(Sudoku sudoku) {
+    prepareFileStructure(SUDOKUFILENAME);
+
+    try (BufferedWriter writer = new BufferedWriter(new FileWriter(getFullFilePath(SUDOKUFILENAME), true))) {
+      String saveId = String.format("SAVE_ID=%s", UUID.randomUUID());
+      writer.append(saveId);
+      writer.newLine();
+
+      String sudokuId = String.format("ID=%s", sudoku.getId());
+      writer.append(sudokuId);
+      writer.newLine();
+
+      String diff = "Difficulty=";
+      if (sudoku.getDifficulty() != null) {
+        diff = String.format("Difficulty=%s", sudoku.getDifficulty().getName());
+      }
+      writer.append(diff);
+      writer.newLine();
+
+      StringBuilder tmpGameField = new StringBuilder();
+      tmpGameField.append("GameField=");
+
+      int sudokuLineLength = sudoku.getGameField().length();
+      for (int i = 0; i < sudokuLineLength; i++) {
+        for (int j = 0; j < sudokuLineLength; j++) {
+          tmpGameField.append(sudoku.getGameField().sudokuArray()[i][j]);
+          tmpGameField.append(",");
+        }
+      }
+      tmpGameField.deleteCharAt(tmpGameField.length() - 1);
+      writer.append(tmpGameField.toString());
+      writer.newLine();
+
+      StringBuilder tmpInitGameField = new StringBuilder();
+      tmpInitGameField.append("InitGameField=");
+      for (int i = 0; i < sudokuLineLength; i++) {
+        for (int j = 0; j < sudokuLineLength; j++) {
+          tmpInitGameField.append(sudoku.getInitialGameField().sudokuArray()[i][j]);
+          tmpInitGameField.append(",");
+        }
+      }
+      tmpInitGameField.deleteCharAt(tmpInitGameField.length() - 1);
+      writer.append(tmpInitGameField.toString());
+      writer.newLine();
+
+      StringBuilder tmpSolvedGameField = new StringBuilder();
+      tmpSolvedGameField.append("SolvedGameField=");
+      for (int i = 0; i < sudokuLineLength; i++) {
+        for (int j = 0; j < sudokuLineLength; j++) {
+          tmpSolvedGameField.append(sudoku.getSolvedGameField().sudokuArray()[i][j]);
+          tmpSolvedGameField.append(",");
+        }
+      }
+      tmpSolvedGameField.deleteCharAt(tmpSolvedGameField.length() - 1);
+      writer.append(tmpSolvedGameField.toString());
+      writer.newLine();
+    } catch (IOException e) {
+      System.err.println(e.getMessage());
     }
 
-    @Override
-    public void saveSudoku(Sudoku sudoku) {
-        prepareFileStructure(SUDOKUFILENAME);
+  }
 
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(getFullFilePath(SUDOKUFILENAME), true))) {
-            String saveId = String.format("SAVE_ID=%s", UUID.randomUUID());
-            writer.append(saveId);
-            writer.newLine();
+  @Override
+  public List<SudokuSaveEntry> getAllSudokus() {
+    List<SudokuSaveEntry> sudokuList = new ArrayList<>();
 
-            String sudokuId = String.format("ID=%s", sudoku.getId());
-            writer.append(sudokuId);
-            writer.newLine();
+    try (BufferedReader br = new BufferedReader(new FileReader(getFullFilePath(SUDOKUFILENAME)))) {
+      String line = br.readLine();
 
-            String diff = "Difficulty=";
-            if (sudoku.getDifficulty() != null) {
-                diff = String.format("Difficulty=%s", sudoku.getDifficulty().getName());
-            }
-            writer.append(diff);
-            writer.newLine();
-
-            StringBuilder tmpGameField = new StringBuilder();
-            tmpGameField.append("GameField=");
-
-            int sudokuLineLength = sudoku.getGameField().length();
-            for (int i = 0; i < sudokuLineLength; i++) {
-                for (int j = 0; j < sudokuLineLength; j++) {
-                    tmpGameField.append(sudoku.getGameField().sudokuArray()[i][j]);
-                    tmpGameField.append(",");
-                }
-            }
-            tmpGameField.deleteCharAt(tmpGameField.length() - 1);
-            writer.append(tmpGameField.toString());
-            writer.newLine();
-
-            StringBuilder tmpInitGameField = new StringBuilder();
-            tmpInitGameField.append("InitGameField=");
-            for (int i = 0; i < sudokuLineLength; i++) {
-                for (int j = 0; j < sudokuLineLength; j++) {
-                    tmpInitGameField.append(sudoku.getInitialGameField().sudokuArray()[i][j]);
-                    tmpInitGameField.append(",");
-                }
-            }
-            tmpInitGameField.deleteCharAt(tmpInitGameField.length() - 1);
-            writer.append(tmpInitGameField.toString());
-            writer.newLine();
-
-            StringBuilder tmpSolvedGameField = new StringBuilder();
-            tmpSolvedGameField.append("SolvedGameField=");
-            for (int i = 0; i < sudokuLineLength; i++) {
-                for (int j = 0; j < sudokuLineLength; j++) {
-                    tmpSolvedGameField.append(sudoku.getSolvedGameField().sudokuArray()[i][j]);
-                    tmpSolvedGameField.append(",");
-                }
-            }
-            tmpSolvedGameField.deleteCharAt(tmpSolvedGameField.length() - 1);
-            writer.append(tmpSolvedGameField.toString());
-            writer.newLine();
-        } catch (IOException e) {
-            System.err.println(e.getMessage());
+      while (line != null) {
+        if (!line.contains("SAVE_ID=")) {
+          line = br.readLine();
+          continue;
         }
-
-    }
-
-    @Override
-    public List<SudokuSaveEntry> getAllSudokus() {
-        List<SudokuSaveEntry> sudokuList = new ArrayList<>();
-
-        try (BufferedReader br = new BufferedReader(new FileReader(getFullFilePath(SUDOKUFILENAME)))) {
-            String line = br.readLine();
-
-            while (line != null) {
-                if (!line.contains("SAVE_ID=")) {
-                    line = br.readLine();
-                    continue;
-                }
-                String[] idArray = line.split("=");
-                String foundSaveId = idArray[1];
-                String sudokuIdLine = br.readLine();
-                String difficultyLine = br.readLine();
-                String gameFieldLine = br.readLine();
-                String initialGameFieldLine = br.readLine();
-                String solvedGameFieldLine = br.readLine();
-
-                Sudoku foundSudoku = new Sudoku(
-                        readSudokuId(sudokuIdLine),
-                        readGameField(gameFieldLine),
-                        readGameField(initialGameFieldLine),
-                        readGameField(solvedGameFieldLine),
-                        extractDifficulty(difficultyLine));
-                sudokuList.add(new SudokuSaveEntry(foundSaveId, foundSudoku, new DateWrapper()));
-                line = br.readLine();
-            }
-        } catch (IOException e) {
-            return Collections.emptyList();
-        }
-        return sudokuList;
-    }
-
-    @Override
-    public Optional<SudokuSaveEntry> getSudoku(String saveId) {
-
-        try (BufferedReader br = new BufferedReader(new FileReader(getFullFilePath(SUDOKUFILENAME)))) {
-            String line = br.readLine();
-
-            while (line != null) {
-                if (!line.contains("SAVE_ID=")) {
-                    line = br.readLine();
-                    continue;
-                }
-                String[] idArray = line.split("=");
-                String foundSaveId = idArray[1];
-                String sudokuLineId = br.readLine();
-                String difficultyLine = br.readLine();
-                String gameFieldLine = br.readLine();
-                String initialGameFieldLine = br.readLine();
-                String solvedGameFieldLine = br.readLine();
-
-                if (saveId.equals(foundSaveId)) {
-                    Sudoku foundSudoku = new Sudoku(
-                            readSudokuId(sudokuLineId),
-                            readGameField(gameFieldLine),
-                            readGameField(initialGameFieldLine),
-                            readGameField(solvedGameFieldLine),
-                            extractDifficulty(difficultyLine));
-
-                    return Optional.of(new SudokuSaveEntry(foundSaveId, foundSudoku, new DateWrapper()));
-                }
-                line = br.readLine();
-            }
-            System.out.println("Sudoku not found!");
-        } catch (IOException e) {
-            return Optional.empty();
-        }
-        return Optional.empty();
-    }
-
-    @Override
-    public void deleteSudoku(String saveId) {
-        File inFile = new File(getFullFilePath(SUDOKUFILENAME));
-        File tmpFile = new File(inFile.getAbsolutePath() + ".tmp");
-        try (BufferedReader br = new BufferedReader(new FileReader(getFullFilePath(SUDOKUFILENAME)));
-             PrintWriter pw = new PrintWriter(new FileWriter(tmpFile))) {
-
-            String line = br.readLine();
-            while (line != null) {
-                if (!line.trim().equals("SAVE_ID=" + saveId)) {
-                    pw.println(line);
-                    pw.flush();
-                } else {
-                    line = br.readLine();
-                    line = br.readLine();
-                    line = br.readLine();
-                    line = br.readLine();
-                    line = br.readLine();
-                }
-                line = br.readLine();
-            }
-        } catch (IOException e) {
-            System.out.println("Error occurred while deleting sudoku.");
-        }
-
-        try {
-            Files.delete(inFile.toPath());
-        } catch (IOException e) {
-            System.out.println("Could not delete file.");
-        }
-
-        //Rename the new file to the filename the original file had.
-        if (!tmpFile.renameTo(inFile)) {
-            System.out.println("Could not rename file");
-        }
-
-    }
-
-    private Difficulty extractDifficulty(String line) {
-        String[] diffArray = line.split("=");
-        String tmpDiff = "";
-        if (diffArray.length > 1) {
-            tmpDiff = diffArray[1];
-        }
-        String finalTmpDiff = tmpDiff;
-        return Difficulty.stream()
-                .filter(d -> d.match(finalTmpDiff))
-                .findFirst()
-                .orElse(null);
-    }
-
-    private SudokuArray readGameField(String gameFieldLine) {
-
-        String[] array = gameFieldLine.split("=");
-        String[] fieldArray = array[1].split(",");
-        int lineLength = (int) Math.sqrt(fieldArray.length);
-        SudokuArray tmpGameField = new SudokuArray(new int[lineLength][lineLength]);
-
-        int fieldCount = 0;
-        for (int i = 0; i < lineLength; i++) {
-            for (int j = 0; j < lineLength; j++) {
-                tmpGameField.sudokuArray()[i][j] = Integer.parseInt(fieldArray[fieldCount]);
-                fieldCount++;
-            }
-        }
-        return tmpGameField;
-    }
-
-    private String readSudokuId(String line) {
         String[] idArray = line.split("=");
-        return idArray[1];
+        String foundSaveId = idArray[1];
+        String sudokuIdLine = br.readLine();
+        String difficultyLine = br.readLine();
+        String gameFieldLine = br.readLine();
+        String initialGameFieldLine = br.readLine();
+        String solvedGameFieldLine = br.readLine();
+
+        Sudoku foundSudoku = SudokuBuilder
+            .withGameField(readGameField(gameFieldLine))
+            .withId(readSudokuId(sudokuIdLine))
+            .withInitialGameField(readGameField(initialGameFieldLine))
+            .withSolvedGameField(readGameField(solvedGameFieldLine))
+            .withDifficulty(extractDifficulty(difficultyLine)).build();
+        sudokuList.add(new SudokuSaveEntry(foundSaveId, foundSudoku, new DateWrapper()));
+        line = br.readLine();
+      }
+    } catch (IOException e) {
+      return Collections.emptyList();
     }
+    return sudokuList;
+  }
+
+  @Override
+  public Optional<SudokuSaveEntry> getSudoku(String saveId) {
+
+    try (BufferedReader br = new BufferedReader(new FileReader(getFullFilePath(SUDOKUFILENAME)))) {
+      String line = br.readLine();
+
+      while (line != null) {
+        if (!line.contains("SAVE_ID=")) {
+          line = br.readLine();
+          continue;
+        }
+        String[] idArray = line.split("=");
+        String foundSaveId = idArray[1];
+        String sudokuLineId = br.readLine();
+        String difficultyLine = br.readLine();
+        String gameFieldLine = br.readLine();
+        String initialGameFieldLine = br.readLine();
+        String solvedGameFieldLine = br.readLine();
+
+        if (saveId.equals(foundSaveId)) {
+          Sudoku foundSudoku = SudokuBuilder
+              .withGameField(readGameField(gameFieldLine))
+              .withId(readSudokuId(sudokuLineId))
+              .withInitialGameField(readGameField(initialGameFieldLine))
+              .withSolvedGameField(readGameField(solvedGameFieldLine))
+              .withDifficulty(extractDifficulty(difficultyLine)).build();
+
+          return Optional.of(new SudokuSaveEntry(foundSaveId, foundSudoku, new DateWrapper()));
+        }
+        line = br.readLine();
+      }
+      System.out.println("Sudoku not found!");
+    } catch (IOException e) {
+      return Optional.empty();
+    }
+    return Optional.empty();
+  }
+
+  @Override
+  public void deleteSudoku(String saveId) {
+    File inFile = new File(getFullFilePath(SUDOKUFILENAME));
+    File tmpFile = new File(inFile.getAbsolutePath() + ".tmp");
+    try (BufferedReader br = new BufferedReader(new FileReader(getFullFilePath(SUDOKUFILENAME)));
+        PrintWriter pw = new PrintWriter(new FileWriter(tmpFile))) {
+
+      String line = br.readLine();
+      while (line != null) {
+        if (!line.trim().equals("SAVE_ID=" + saveId)) {
+          pw.println(line);
+          pw.flush();
+        } else {
+          line = br.readLine();
+          line = br.readLine();
+          line = br.readLine();
+          line = br.readLine();
+          line = br.readLine();
+        }
+        line = br.readLine();
+      }
+    } catch (IOException e) {
+      System.out.println("Error occurred while deleting sudoku.");
+    }
+
+    try {
+      Files.delete(inFile.toPath());
+    } catch (IOException e) {
+      System.out.println("Could not delete file.");
+    }
+
+    //Rename the new file to the filename the original file had.
+    if (!tmpFile.renameTo(inFile)) {
+      System.out.println("Could not rename file");
+    }
+
+  }
+
+  private Difficulty extractDifficulty(String line) {
+    String[] diffArray = line.split("=");
+    String tmpDiff = "";
+    if (diffArray.length > 1) {
+      tmpDiff = diffArray[1];
+    }
+    String finalTmpDiff = tmpDiff;
+    return Difficulty.stream()
+        .filter(d -> d.match(finalTmpDiff))
+        .findFirst()
+        .orElse(null);
+  }
+
+  private SudokuArray readGameField(String gameFieldLine) {
+
+    String[] array = gameFieldLine.split("=");
+    String[] fieldArray = array[1].split(",");
+    int lineLength = (int) Math.sqrt(fieldArray.length);
+    SudokuArray tmpGameField = new SudokuArray(new int[lineLength][lineLength]);
+
+    int fieldCount = 0;
+    for (int i = 0; i < lineLength; i++) {
+      for (int j = 0; j < lineLength; j++) {
+        tmpGameField.sudokuArray()[i][j] = Integer.parseInt(fieldArray[fieldCount]);
+        fieldCount++;
+      }
+    }
+    return tmpGameField;
+  }
+
+  private String readSudokuId(String line) {
+    String[] idArray = line.split("=");
+    return idArray[1];
+  }
 
 }
--- src/main/java/de/dhbw/karlsruhe/domain/models/Sudoku.java
+++ src/main/java/de/dhbw/karlsruhe/domain/models/Sudoku.java
@@ -1,101 +1,90 @@
 package de.dhbw.karlsruhe.domain.models;
 
 import de.dhbw.karlsruhe.domain.models.wrapper.SudokuArray;
-
 import java.util.Arrays;
 import java.util.Objects;
-import java.util.UUID;
 
 public class Sudoku {
 
-    private String id;
-    private SudokuArray gameField;
-    private SudokuArray initialGameField;
-    private SudokuArray solvedGameField;
-    private SudokuChange lastChange;
-    private Difficulty difficulty;
+  private String id;
+  private SudokuArray gameField;
+  private SudokuArray initialGameField;
+  private SudokuArray solvedGameField;
+  private SudokuChange lastChange;
+  private Difficulty difficulty;
 
-    public Sudoku(SudokuSize size) {
-        id = UUID.randomUUID().toString();
-        gameField = new SudokuArray(new int[size.size][size.size]);
-        initialGameField = new SudokuArray(new int[size.size][size.size]);
-        solvedGameField = new SudokuArray(new int[size.size][size.size]);
-        difficulty = Difficulty.EASY;
-        lastChange = null;
-    }
+  public Sudoku(String id, SudokuArray gameField, SudokuArray initialGameField, SudokuArray solvedGameField,
+      Difficulty difficulty) {
+    this.id = id;
+    this.gameField = gameField;
+    this.initialGameField = initialGameField;
+    this.solvedGameField = solvedGameField;
+    this.difficulty = difficulty;
+    lastChange = null;
+  }
 
-    public Sudoku(SudokuSize size, Difficulty difficulty) {
-        id = UUID.randomUUID().toString();
-        gameField = new SudokuArray(new int[size.size][size.size]);
-        this.difficulty = difficulty;
-        lastChange = null;
-    }
+  public SudokuArray getInitialGameField() {
+    return initialGameField;
+  }
 
-    public Sudoku(String id, SudokuArray gameField, SudokuArray initialGameField, SudokuArray solvedGameField, Difficulty difficulty) {
-        this.id = id;
-        this.gameField = gameField;
-        this.initialGameField = initialGameField;
-        this.solvedGameField = solvedGameField;
-        this.difficulty = difficulty;
-        lastChange = null;
-    }
+  public SudokuArray getSolvedGameField() {
+    return solvedGameField;
+  }
 
-    public SudokuArray getInitialGameField() {
-        return initialGameField;
+  public void setInitialGameField(SudokuArray initialGameField) {
+    if (this.initialGameField == null) {
+      this.initialGameField = initialGameField;
     }
+  }
 
-    public SudokuArray getSolvedGameField() {
-        return solvedGameField;
+  public void setSolvedGameField(SudokuArray solvedGameField) {
+    if (this.initialGameField == null) {
+      this.solvedGameField = solvedGameField;
     }
+  }
 
-    public void setInitialGameField(SudokuArray initialGameField) {
-        if (this.initialGameField == null) {
-            this.initialGameField = initialGameField;
-        }
+  public boolean setField(int row, int col, int val) {
+    if (this.initialGameField.sudokuArray()[row][col] == 0) {
+      this.lastChange = new SudokuChange(row, col, this.gameField.sudokuArray()[row][col], val);
+      this.gameField.sudokuArray()[row][col] = val;
+      return true;
     }
+    return false;
+  }
 
-    public void setSolvedGameField(SudokuArray solvedGameField) {
-        if (this.initialGameField == null) {
-            this.solvedGameField = solvedGameField;
-        }
-    }
+  public Difficulty getDifficulty() {
+    return difficulty;
+  }
 
-    public boolean setField(int row, int col, int val) {
-        if (this.initialGameField.sudokuArray()[row][col] == 0) {
-            this.lastChange = new SudokuChange(row,col,this.gameField.sudokuArray()[row][col], val);
-            this.gameField.sudokuArray()[row][col] = val;
-            return true;
-        }
-        return false;
-    }
+  public String getId() {
+    return id;
+  }
 
-    public Difficulty getDifficulty() {
-        return difficulty;
-    }
+  public SudokuArray getGameField() {
+    return this.gameField;
+  }
 
-    public String getId() {
-        return id;
-    }
+  public SudokuChange getLastChange() {
+    return lastChange;
+  }
 
-    public SudokuArray getGameField(){
-        return this.gameField;
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
     }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    Sudoku sudoku = (Sudoku) o;
+    return id.equals(sudoku.id) && Arrays.deepEquals(gameField.sudokuArray(), sudoku.gameField.sudokuArray())
+        && Arrays.deepEquals(initialGameField.sudokuArray(), sudoku.initialGameField.sudokuArray())
+        && difficulty == sudoku.difficulty;
+  }
 
-    public SudokuChange getLastChange() {
-        return lastChange;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Sudoku sudoku = (Sudoku) o;
-        return id.equals(sudoku.id) && Arrays.deepEquals(gameField.sudokuArray(), sudoku.gameField.sudokuArray()) && Arrays.deepEquals(initialGameField.sudokuArray(), sudoku.initialGameField.sudokuArray()) && difficulty == sudoku.difficulty;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, gameField, initialGameField, difficulty);
-    }
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, gameField, initialGameField, difficulty);
+  }
 
 }
