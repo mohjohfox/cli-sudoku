266a968180c05454c862c7841bb7f92285f55042
mohjohfox <marcorobinf@gmail.com>
2023-05-29 12:01:06 +0200

    WIP - add missing examples

--- src/main/java/de/dhbw/karlsruhe/domain/services/DurationTrackService.java
+++ src/main/java/de/dhbw/karlsruhe/domain/services/DurationTrackService.java
@@ -1,60 +1,61 @@
 package de.dhbw.karlsruhe.domain.services;
 
+import de.dhbw.karlsruhe.adapters.persistence.DurationTrackAdapter;
+import de.dhbw.karlsruhe.domain.Location;
 import de.dhbw.karlsruhe.domain.models.DurationTrackSaveEntry;
-import de.dhbw.karlsruhe.domain.ports.persistence.DurationTrackPort;
-
 import java.util.UUID;
 
 public class DurationTrackService {
 
-    private long startTimeMillis;
-    private long endTimeMillis;
-    private long durationMillis;
-    private DurationTrackPort durationTrackPort;
-    private DurationTrackSaveEntry durationTrackSaveEntry;
-    private UUID saveId;
+  private long startTimeMillis;
+  private long endTimeMillis;
+  private long durationMillis;
+  private DurationTrackAdapter durationTrackAdapter;
+  private DurationTrackSaveEntry durationTrackSaveEntry;
+  private UUID saveId;
 
-    public DurationTrackService() {
-        this.durationTrackPort = DependencyFactory.getInstance().getDependency(DurationTrackPort.class);
+  public DurationTrackService() {
+    this.durationTrackAdapter = new DurationTrackAdapter(Location.PROD);
 
-        this.startTimeMillis = -1;
-        this.endTimeMillis = -1;
+    this.startTimeMillis = -1;
+    this.endTimeMillis = -1;
+  }
+
+  public void setStartTime() {
+    this.startTimeMillis = System.currentTimeMillis();
+  }
+
+  public void setEndTime(String sudokuID) {
+    long previousDuration = this.loadDuration(sudokuID);
+
+    this.endTimeMillis = System.currentTimeMillis();
+    this.calculateDuration();
+
+    this.durationMillis += previousDuration;
+  }
+
+  public void saveDuration(String sudokuID) {
+    this.saveId = UUID.randomUUID();
+    this.durationTrackSaveEntry = new DurationTrackSaveEntry(String.valueOf(this.saveId), sudokuID,
+        this.durationMillis);
+    this.durationTrackAdapter.saveSolvingTime(this.durationTrackSaveEntry);
+  }
+
+  private long loadDuration(String sudokuId) {
+    return this.durationTrackAdapter.loadSolvingTime(sudokuId);
+  }
+
+  private void calculateDuration() {
+    if (this.validateTimestamps()) {
+      this.durationMillis = this.endTimeMillis - this.startTimeMillis;
+    }
+  }
+
+  private boolean validateTimestamps() {
+    if (this.startTimeMillis == -1 || this.endTimeMillis == -1) {
+      return false;
     }
 
-    public void setStartTime() {
-        this.startTimeMillis = System.currentTimeMillis();
-    }
-
-    public void setEndTime(String sudokuID) {
-        long previousDuration = this.loadDuration(sudokuID);
-
-        this.endTimeMillis = System.currentTimeMillis();
-        this.calculateDuration();
-
-        this.durationMillis += previousDuration;
-    }
-
-    public void saveDuration(String sudokuID) {
-        this.saveId = UUID.randomUUID();
-        this.durationTrackSaveEntry = new DurationTrackSaveEntry(String.valueOf(this.saveId), sudokuID, this.durationMillis);
-        this.durationTrackPort.saveSolvingTime(this.durationTrackSaveEntry);
-    }
-
-    private long loadDuration(String sudokuId) {
-        return this.durationTrackPort.loadSolvingTime(sudokuId);
-    }
-
-    private void calculateDuration() {
-        if (this.validateTimestamps()) {
-            this.durationMillis = this.endTimeMillis - this.startTimeMillis;
-        }
-    }
-
-    private boolean validateTimestamps() {
-        if (this.startTimeMillis == -1 || this.endTimeMillis == -1) {
-            return false;
-        }
-
-        return this.endTimeMillis > this.startTimeMillis;
-    }
+    return this.endTimeMillis > this.startTimeMillis;
+  }
 }
