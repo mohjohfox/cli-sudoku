56ead136bb04a99800f318834c05c6b6957e4c16
Alisa Fien <alisa1fien@gmail.com>
2023-05-30 15:55:47 +0200

    Added Test

--- src/test/java/de/dhbw/karlsruhe/services/SudokuValidatorTest.java
+++ src/test/java/de/dhbw/karlsruhe/services/SudokuValidatorTest.java
@@ -28,8 +28,31 @@ public class SudokuValidatorTest {
         Sudoku sudoku = new MockValidFilledSudoku(null, new SudokuArray(gameField),new SudokuArray(gameField),new SudokuArray(gameField), Difficulty.EASY);
         int[][] sudokuArray = sudoku.getGameField().sudokuArray();
         assertFalse(sudokuValidatorService.isSudokuNotFullyFilled(sudokuArray));
-        
+
         sudokuArray[0][0] = 0;
         assertTrue(sudokuValidatorService.isSudokuNotFullyFilled(sudokuArray));
+    }
+
+    @Test
+    void crossCheckTest(){
+        SudokuValidatorService sudokuValidatorService = new SudokuValidatorService();
+        int[][] gameField = new int[9][9];
+        Sudoku sudoku = new MockValidFilledSudoku(null, new SudokuArray(gameField),new SudokuArray(gameField),new SudokuArray(gameField), Difficulty.EASY);
+        int[][] sudokuArray = sudoku.getGameField().sudokuArray();
+        int[][] emptyField = new int[9][9];
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                emptyField[i][j] = 0;
+            }
+        }
+
+        SudokuArray emptySudokuArray = new SudokuArray(emptyField);
+        assertTrue(sudokuValidatorService.crossCheck(sudoku.getGameField(), emptySudokuArray, sudoku.getGameField()).size() == 0);
+
+        sudokuArray[0][0] = 0;
+        assertTrue(sudokuValidatorService.crossCheck(new SudokuArray(sudokuArray), emptySudokuArray, sudoku.getGameField()).size() == 0);
+
+        sudokuArray[0][0] = 1;
+        assertTrue(sudokuValidatorService.crossCheck(new SudokuArray(sudokuArray), emptySudokuArray, sudoku.getGameField()).size() == 1);
     }
 }
