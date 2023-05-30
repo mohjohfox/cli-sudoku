5410746a1090b96101236fdd944b5721ae7ee8f6
Marcel <rhyperio.yt@gmail.com>
2023-05-30 09:09:04 +0200

    Add math problem object

---
+++ src/main/java/de/dhbw/karlsruhe/domain/models/MathProblem.java
@@ -0,0 +1,38 @@
+package de.dhbw.karlsruhe.domain.models;
+
+public class MathProblem {
+
+    private String problemAsText;
+    private int[] operands;
+    private int result;
+
+    public MathProblem(String problemAsText, int[] operands, int result) {
+        this.setProblemAsText(problemAsText);
+        this.setOperands(operands);
+        this.setResult(result);
+    }
+
+    public String getProblemAsText() {
+        return problemAsText;
+    }
+
+    public void setProblemAsText(String problemAsText) {
+        this.problemAsText = problemAsText;
+    }
+
+    public int[] getOperands() {
+        return operands;
+    }
+
+    public void setOperands(int[] operands) {
+        this.operands = operands;
+    }
+
+    public int getResult() {
+        return result;
+    }
+
+    public void setResult(int result) {
+        this.result = result;
+    }
+}
