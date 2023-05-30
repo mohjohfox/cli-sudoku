eefb60fce6be044586a8ec4a3127fcc32a286d0f
Alisa Fien <alisa1fien@gmail.com>
2023-05-30 15:29:15 +0200

    Added Test

--- src/test/java/de/dhbw/karlsruhe/services/SudokuValidatorTest.java
+++ src/test/java/de/dhbw/karlsruhe/services/SudokuValidatorTest.java
@@ -7,6 +7,7 @@ import de.dhbw.karlsruhe.domain.services.SudokuValidatorService;
 import de.dhbw.karlsruhe.services.mocks.MockValidFilledSudoku;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SudokuValidatorTest {
@@ -18,5 +19,17 @@ public class SudokuValidatorTest {
         Sudoku sudoku = new MockValidFilledSudoku(null, new SudokuArray(gameField),new SudokuArray(gameField),new SudokuArray(gameField), Difficulty.EASY);
         int[][] sudokuArray = sudoku.getGameField().sudokuArray();
         assertTrue(sudokuValidatorService.isSudokuValid(sudokuArray));
+    }
+
+    @Test
+    void isSudokuNotFullyFilledTest(){
+        SudokuValidatorService sudokuValidatorService = new SudokuValidatorService();
+        int[][] gameField = new int[9][9];
+        Sudoku sudoku = new MockValidFilledSudoku(null, new SudokuArray(gameField),new SudokuArray(gameField),new SudokuArray(gameField), Difficulty.EASY);
+        int[][] sudokuArray = sudoku.getGameField().sudokuArray();
+        assertFalse(sudokuValidatorService.isSudokuNotFullyFilled(sudokuArray));
+        
+        sudokuArray[0][0] = 0;
+        assertTrue(sudokuValidatorService.isSudokuNotFullyFilled(sudokuArray));
     }
 }
