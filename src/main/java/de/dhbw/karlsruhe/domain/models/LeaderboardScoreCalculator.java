48c65af7d09a598f512b462f69e71846943cf5e0
mohjohfox <marcofuchs@mohjohfox.de>
2023-05-31 16:53:00 +0200

    Fixed Enum comparing

--- src/main/java/de/dhbw/karlsruhe/domain/models/LeaderboardScoreCalculator.java
+++ src/main/java/de/dhbw/karlsruhe/domain/models/LeaderboardScoreCalculator.java
@@ -2,78 +2,80 @@ package de.dhbw.karlsruhe.domain.models;
 
 public class LeaderboardScoreCalculator {
 
-    public LeaderboardScoreCalculator() {
+  public LeaderboardScoreCalculator() {
 
+  }
+
+  public int calculateCompleteLeaderboardScore(String[] unsolvedOrWrongFields, boolean isCorrect, long timeInMillis,
+      Difficulty difficulty) {
+    int score = 0;
+    int difficultyAsInt = 0;
+    int scoreForSolvedFields;
+
+    if (difficulty == Difficulty.EASY) {
+      difficultyAsInt = 1;
+    } else if (difficulty == Difficulty.MEDIUM) {
+      difficultyAsInt = 2;
+    } else {
+      difficultyAsInt = 3;
     }
 
-    public int calculateCompleteLeaderboardScore(String[] unsolvedOrWrongFields, boolean isCorrect, long timeInMillis, String difficultyAsString) {
-        int score = 0;
-        int difficultyAsInt = 0;
-        int scoreForSolvedFields;
-
-        if (difficultyAsString.equals(Difficulty.EASY.getName())) {
-            difficultyAsInt = 1;
-        } else if (difficultyAsString.equals(Difficulty.MEDIUM.getName())) {
-            difficultyAsInt = 2;
-        } else {
-            difficultyAsInt = 3;
-        }
-
-        if (isCorrect) {
-            score += 250;
-        } else {
-            scoreForSolvedFields = this.calculateScoreForUnsolvedOrWrongFields(unsolvedOrWrongFields, isCorrect);
-            score += scoreForSolvedFields;
-        }
-
-        score += difficultyAsInt * 0.75 * 6;
-        score += (System.currentTimeMillis() - timeInMillis) * 0.000000001;
-
-        return score;
+    if (isCorrect) {
+      score += 250;
+    } else {
+      scoreForSolvedFields = this.calculateScoreForUnsolvedOrWrongFields(unsolvedOrWrongFields, isCorrect);
+      score += scoreForSolvedFields;
     }
 
-    public int calculateTimeLeaderboardScore(String[] unsolvedOrWrongFields, boolean isCorrect, long timeInMillis) {
-        int score = 0;
-        int scoreForSolvedFields = 0;
+    score += difficultyAsInt * 0.75 * 6;
+    score += (System.currentTimeMillis() - timeInMillis) * 0.000000001;
 
-        score += (System.currentTimeMillis() - timeInMillis) * 0.00000000001;
+    return score;
+  }
 
-        scoreForSolvedFields = this.calculateScoreForUnsolvedOrWrongFields(unsolvedOrWrongFields, isCorrect);
-        score += scoreForSolvedFields;
+  public int calculateTimeLeaderboardScore(String[] unsolvedOrWrongFields, boolean isCorrect, long timeInMillis) {
+    int score = 0;
+    int scoreForSolvedFields = 0;
 
-        return score;
+    score += (System.currentTimeMillis() - timeInMillis) * 0.00000000001;
+
+    scoreForSolvedFields = this.calculateScoreForUnsolvedOrWrongFields(unsolvedOrWrongFields, isCorrect);
+    score += scoreForSolvedFields;
+
+    return score;
+  }
+
+  public int calculateDifficultyLeaderboardScore(String[] unsolvedOrWrongFields, boolean isCorrect,
+      Difficulty difficulty) {
+    int score = 0;
+    int difficultyAsInt = 0;
+    int scoreForSolvedFields = 0;
+
+    if (difficulty == Difficulty.EASY) {
+      difficultyAsInt = 1;
+    } else if (difficulty == Difficulty.MEDIUM) {
+      difficultyAsInt = 2;
+    } else {
+      difficultyAsInt = 3;
     }
 
-    public int calculateDifficultyLeaderboardScore(String[] unsolvedOrWrongFields, boolean isCorrect, String difficultyAsString) {
-        int score = 0;
-        int difficultyAsInt = 0;
-        int scoreForSolvedFields = 0;
+    score += difficultyAsInt * 0.75 * 6;
 
-        if (difficultyAsString.equals(Difficulty.EASY.getName())) {
-            difficultyAsInt = 1;
-        } else if (difficultyAsString.equals(Difficulty.MEDIUM.getName())) {
-            difficultyAsInt = 2;
-        } else {
-            difficultyAsInt = 3;
-        }
+    scoreForSolvedFields = this.calculateScoreForUnsolvedOrWrongFields(unsolvedOrWrongFields, isCorrect);
+    score += scoreForSolvedFields;
 
-        score += difficultyAsInt * 0.75 * 6;
+    return score;
+  }
 
-        scoreForSolvedFields = this.calculateScoreForUnsolvedOrWrongFields(unsolvedOrWrongFields, isCorrect);
-        score += scoreForSolvedFields;
+  private int calculateScoreForUnsolvedOrWrongFields(String[] unsolvedOrWrongFields, boolean isCorrect) {
+    int initialScore = 250;
 
-        return score;
+    if (isCorrect) {
+      return initialScore;
     }
 
-    private int calculateScoreForUnsolvedOrWrongFields(String[] unsolvedOrWrongFields, boolean isCorrect) {
-        int initialScore = 250;
+    int negativeScore = unsolvedOrWrongFields.length * 15;
 
-        if (isCorrect) {
-            return  initialScore;
-        }
-
-        int negativeScore = unsolvedOrWrongFields.length * 15;
-
-        return initialScore - negativeScore;
-    }
+    return initialScore - negativeScore;
+  }
 }
--- src/main/java/de/dhbw/karlsruhe/domain/services/dialogs/PlayDialogService.java
+++ src/main/java/de/dhbw/karlsruhe/domain/services/dialogs/PlayDialogService.java
@@ -1,152 +1,162 @@
 package de.dhbw.karlsruhe.domain.services.dialogs;
 
-import de.dhbw.karlsruhe.domain.models.*;
 import de.dhbw.karlsruhe.adapters.cli.input.InvalidInputException;
 import de.dhbw.karlsruhe.domain.models.Difficulty;
+import de.dhbw.karlsruhe.domain.models.DurationTrackSaveEntry;
+import de.dhbw.karlsruhe.domain.models.GameInformation;
+import de.dhbw.karlsruhe.domain.models.Leaderboard;
+import de.dhbw.karlsruhe.domain.models.LeaderboardScoreCalculator;
 import de.dhbw.karlsruhe.domain.models.Sudoku;
 import de.dhbw.karlsruhe.domain.models.SudokuSize;
 import de.dhbw.karlsruhe.domain.models.generation.SudokuGeneratorBacktracking;
 import de.dhbw.karlsruhe.domain.models.generation.SudokuGeneratorTransformation;
 import de.dhbw.karlsruhe.domain.models.play.actions.PlayAction;
-import de.dhbw.karlsruhe.domain.models.wrapper.SudokuArray;
 import de.dhbw.karlsruhe.domain.ports.dialogs.input.InputPort;
 import de.dhbw.karlsruhe.domain.ports.dialogs.input.PlayInputPort;
 import de.dhbw.karlsruhe.domain.ports.dialogs.output.PlayOutputPort;
 import de.dhbw.karlsruhe.domain.ports.dialogs.output.SudokuOutputPort;
 import de.dhbw.karlsruhe.domain.ports.persistence.LeaderboardStorePort;
-import de.dhbw.karlsruhe.domain.ports.persistence.SudokuPersistencePort;
-import de.dhbw.karlsruhe.domain.ports.persistence.UserPort;
 import de.dhbw.karlsruhe.domain.services.DependencyFactory;
-import de.dhbw.karlsruhe.domain.services.SettingService;
 import de.dhbw.karlsruhe.domain.services.DurationTrackService;
+import de.dhbw.karlsruhe.domain.services.SettingService;
 import de.dhbw.karlsruhe.domain.services.SudokuValidatorService;
-
 import java.util.List;
 import java.util.Random;
 
 public class PlayDialogService {
-    private Sudoku sudoku;
-    private Random rand = new Random();
 
-    private SudokuGeneratorTransformation sgTransformation = DependencyFactory.getInstance().getDependency(SudokuGeneratorTransformation.class);
-    private SudokuGeneratorBacktracking sgBacktracking = DependencyFactory.getInstance().getDependency(SudokuGeneratorBacktracking.class);
-    private SudokuValidatorService sudokuValidator = DependencyFactory.getInstance().getDependency(SudokuValidatorService.class);
-    private SettingService settingService = DependencyFactory.getInstance().getDependency(SettingService.class);
-    private DurationTrackService durationTrackService = DependencyFactory.getInstance().getDependency(DurationTrackService.class);
-    private LeaderboardDialogService leaderboardDialogService = DependencyFactory.getInstance().getDependency(LeaderboardDialogService.class);
-    private LeaderboardScoreCalculator leaderboardScoreCalculator = DependencyFactory.getInstance().getDependency(LeaderboardScoreCalculator.class);
-    private Leaderboard leaderboard = DependencyFactory.getInstance().getDependency(Leaderboard.class);
-    private final InputPort inputPort = DependencyFactory.getInstance().getDependency(InputPort.class);
-    private final PlayInputPort playInputPort = DependencyFactory.getInstance().getDependency(PlayInputPort.class);
-    private final PlayOutputPort outputPort = DependencyFactory.getInstance().getDependency(PlayOutputPort.class);
-    private final SudokuOutputPort sudokuOutputPort = DependencyFactory.getInstance().getDependency(SudokuOutputPort.class);
-    private final LeaderboardStorePort leaderboardStorePort = DependencyFactory.getInstance().getDependency(LeaderboardStorePort .class);
+  private Sudoku sudoku;
+  private Random rand = new Random();
 
-    public PlayDialogService() {
-        // Empty constructor for JSON parser
+  private SudokuGeneratorTransformation sgTransformation = DependencyFactory.getInstance()
+      .getDependency(SudokuGeneratorTransformation.class);
+  private SudokuGeneratorBacktracking sgBacktracking = DependencyFactory.getInstance()
+      .getDependency(SudokuGeneratorBacktracking.class);
+  private SudokuValidatorService sudokuValidator = DependencyFactory.getInstance()
+      .getDependency(SudokuValidatorService.class);
+  private SettingService settingService = DependencyFactory.getInstance().getDependency(SettingService.class);
+  private DurationTrackService durationTrackService = DependencyFactory.getInstance()
+      .getDependency(DurationTrackService.class);
+  private LeaderboardDialogService leaderboardDialogService = DependencyFactory.getInstance()
+      .getDependency(LeaderboardDialogService.class);
+  private LeaderboardScoreCalculator leaderboardScoreCalculator = DependencyFactory.getInstance()
+      .getDependency(LeaderboardScoreCalculator.class);
+  private Leaderboard leaderboard = DependencyFactory.getInstance().getDependency(Leaderboard.class);
+  private final InputPort inputPort = DependencyFactory.getInstance().getDependency(InputPort.class);
+  private final PlayInputPort playInputPort = DependencyFactory.getInstance().getDependency(PlayInputPort.class);
+  private final PlayOutputPort outputPort = DependencyFactory.getInstance().getDependency(PlayOutputPort.class);
+  private final SudokuOutputPort sudokuOutputPort = DependencyFactory.getInstance()
+      .getDependency(SudokuOutputPort.class);
+  private final LeaderboardStorePort leaderboardStorePort = DependencyFactory.getInstance()
+      .getDependency(LeaderboardStorePort.class);
+
+  public PlayDialogService() {
+    // Empty constructor for JSON parser
+  }
+
+  public void startNewStandardGame(Difficulty dif) {
+    if (rand.nextInt() < 0.5) {
+      outputPort.transformedSudoku();
+      sudoku = sgTransformation.generateSudoku(dif);
+    } else {
+      outputPort.backtrackingSudoku();
+      sudoku = sgBacktracking.generateSudoku(SudokuSize.NORMAL, dif);
+    }
+    startGame();
+  }
+
+  public void startNewGame(SudokuSize sudokuSize, Difficulty dif) {
+    if (sudokuSize == SudokuSize.BIG) {
+      outputPort.longGeneratingSudoku();
+      sudoku = sgBacktracking.generateSudoku(SudokuSize.BIG, dif);
+      startGame();
+    }
+    if (sudokuSize == SudokuSize.NORMAL) {
+      startNewStandardGame(dif);
+    }
+    if (sudokuSize == SudokuSize.SMALL) {
+      outputPort.backtrackingSudoku();
+      sudoku = sgBacktracking.generateSudoku(SudokuSize.SMALL, dif);
+      startGame();
+    }
+  }
+
+  public void startSavedGame(Sudoku loadedSudoku) {
+    sudoku = loadedSudoku;
+    startGame();
+  }
+
+  private void startGame() {
+    boolean isGameClosed = false;
+    this.durationTrackService.setStartTime();
+    outputPort.startGame();
+    outputPort.possibleHints(settingService.getSettingFromCurrentUser());
+
+    while (sudokuValidator.isSudokuNotFullyFilled(sudoku.getGameField().sudokuArray())) {
+      sudokuOutputPort.print(sudoku);
+      PlayAction playAction = userInputDialog();
+      playAction.executeAction(this.sudoku);
+      if (playAction.isCloseGame()) {
+        isGameClosed = true;
+        break;
+      }
     }
 
-    public void startNewStandardGame(Difficulty dif) {
-        if (rand.nextInt() < 0.5) {
-            outputPort.transformedSudoku();
-            sudoku = sgTransformation.generateSudoku(dif);
-        } else {
-            outputPort.backtrackingSudoku();
-            sudoku = sgBacktracking.generateSudoku(SudokuSize.NORMAL, dif);
-        }
-        startGame();
+    if (!sudokuValidator.isSudokuNotFullyFilled(sudoku.getGameField().sudokuArray())) {
+      sudokuOutputPort.print(sudoku);
+      sudokuOutputPort.emptyLine();
     }
 
-    public void startNewGame(SudokuSize sudokuSize, Difficulty dif) {
-        if (sudokuSize == SudokuSize.BIG) {
-            outputPort.longGeneratingSudoku();
-            sudoku = sgBacktracking.generateSudoku(SudokuSize.BIG, dif);
-            startGame();
-        }
-        if (sudokuSize == SudokuSize.NORMAL) {
-            startNewStandardGame(dif);
-        }
-        if (sudokuSize == SudokuSize.SMALL) {
-            outputPort.backtrackingSudoku();
-            sudoku = sgBacktracking.generateSudoku(SudokuSize.SMALL, dif);
-            startGame();
-        }
+    // Save game duration
+    this.durationTrackService.setEndTime(sudoku.getId());
+    this.durationTrackService.saveDuration(sudoku.getId());
+
+    if (!isGameClosed) {
+      // Save game score
+      DurationTrackSaveEntry durationTrackSaveEntry = this.durationTrackService.getDurationTrackSaveEntry();
+      boolean isSudokuValid = this.sudokuValidator.isSudokuValid(sudoku.getGameField().sudokuArray());
+      List<String> unsolvedOrWrongFields = this.sudokuValidator.crossCheck(sudoku);
+      String username = GameInformation.username;
+      long durationInMillis = durationTrackSaveEntry.getDuration();
+
+      int scoreComplete = this.leaderboardScoreCalculator.calculateCompleteLeaderboardScore(
+          unsolvedOrWrongFields.toArray(new String[0]), isSudokuValid, durationInMillis, sudoku.getDifficulty());
+      int scoreTime = this.leaderboardScoreCalculator.calculateTimeLeaderboardScore(
+          unsolvedOrWrongFields.toArray(new String[0]), isSudokuValid, durationInMillis);
+      int scoreDifficulty = this.leaderboardScoreCalculator.calculateDifficultyLeaderboardScore(
+          unsolvedOrWrongFields.toArray(new String[0]), isSudokuValid, sudoku.getDifficulty());
+
+      this.leaderboard.addToLeaderboard(1, username, scoreComplete);
+      this.leaderboard.addToLeaderboard(2, username, scoreTime);
+
+      if (Difficulty.EASY == sudoku.getDifficulty()) {
+        this.leaderboard.addToLeaderboard(3, username, scoreDifficulty);
+      } else if (Difficulty.MEDIUM == sudoku.getDifficulty()) {
+        this.leaderboard.addToLeaderboard(4, username, scoreDifficulty);
+      } else {
+        this.leaderboard.addToLeaderboard(5, username, scoreDifficulty);
+      }
+
+      this.leaderboardStorePort.saveLeaderboard(this.leaderboard);
     }
 
-    public void startSavedGame(Sudoku loadedSudoku) {
-        sudoku = loadedSudoku;
-        startGame();
+    if (!sudokuValidator.isSudokuNotFullyFilled(sudoku.getGameField().sudokuArray())) {
+      List<String> notCorrectFields = this.sudokuValidator.crossCheck(sudoku);
+      outputPort.notCorrectSudoku(notCorrectFields);
     }
+  }
 
-    private void startGame() {
-        boolean isGameClosed = false;
-        this.durationTrackService.setStartTime();
-        outputPort.startGame();
+  private PlayAction userInputDialog() {
+    PlayAction playAction = null;
+
+    while (playAction == null) {
+      try {
+        playAction = playInputPort.getPlayAction();
+      } catch (InvalidInputException ex) {
+        outputPort.inputError();
         outputPort.possibleHints(settingService.getSettingFromCurrentUser());
-
-        while (sudokuValidator.isSudokuNotFullyFilled(sudoku.getGameField().sudokuArray())) {
-            sudokuOutputPort.print(sudoku);
-            PlayAction playAction = userInputDialog();
-            playAction.executeAction(this.sudoku);
-            if (playAction.isCloseGame()) {
-                isGameClosed = true;
-                break;
-            }
-        }
-
-        if (!sudokuValidator.isSudokuNotFullyFilled(sudoku.getGameField().sudokuArray())) {
-            sudokuOutputPort.print(sudoku);
-            sudokuOutputPort.emptyLine();
-        }
-
-        // Save game duration
-        this.durationTrackService.setEndTime(sudoku.getId());
-        this.durationTrackService.saveDuration(sudoku.getId());
-
-        if (!isGameClosed) {
-            // Save game score
-            DurationTrackSaveEntry durationTrackSaveEntry = this.durationTrackService.getDurationTrackSaveEntry();
-            boolean isSudokuValid = this.sudokuValidator.isSudokuValid(sudoku.getGameField().sudokuArray());
-            String difficultyAsString = sudoku.getDifficulty().getShortDifficultyName();
-            List<String> unsolvedOrWrongFields = this.sudokuValidator.crossCheck(sudoku);
-            String username = GameInformation.username;
-            long durationInMillis = durationTrackSaveEntry.getDuration();
-
-            int scoreComplete = this.leaderboardScoreCalculator.calculateCompleteLeaderboardScore(unsolvedOrWrongFields.toArray(new String[0]) ,isSudokuValid, durationInMillis, difficultyAsString);
-            int scoreTime = this.leaderboardScoreCalculator.calculateTimeLeaderboardScore(unsolvedOrWrongFields.toArray(new String[0]) ,isSudokuValid, durationInMillis);
-            int scoreDifficulty = this.leaderboardScoreCalculator.calculateDifficultyLeaderboardScore(unsolvedOrWrongFields.toArray(new String[0]) ,isSudokuValid, difficultyAsString);
-
-            this.leaderboard.addToLeaderboard(1, username, scoreComplete);
-            this.leaderboard.addToLeaderboard(2, username, scoreTime);
-
-            if (difficultyAsString.equals(Difficulty.EASY.getShortDifficultyName())) {
-                this.leaderboard.addToLeaderboard(3, username, scoreDifficulty);
-            } else if (difficultyAsString.equals(Difficulty.MEDIUM.getShortDifficultyName())) {
-                this.leaderboard.addToLeaderboard(4, username, scoreDifficulty);
-            } else {
-                this.leaderboard.addToLeaderboard(5, username, scoreDifficulty);
-            }
-
-            this.leaderboardStorePort.saveLeaderboard(this.leaderboard);
-        }
-
-        if (!sudokuValidator.isSudokuNotFullyFilled(sudoku.getGameField().sudokuArray())) {
-            List<String> notCorrectFields = this.sudokuValidator.crossCheck(sudoku);
-            outputPort.notCorrectSudoku(notCorrectFields);
-        }
+      }
     }
-
-    private PlayAction userInputDialog() {
-        PlayAction playAction = null;
-
-        while (playAction == null) {
-            try {
-                playAction = playInputPort.getPlayAction();
-            } catch (InvalidInputException ex) {
-                outputPort.inputError();
-                outputPort.possibleHints(settingService.getSettingFromCurrentUser());
-            }
-        }
-        return playAction;
-    }
+    return playAction;
+  }
 }
